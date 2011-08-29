d1501126cfa38f951cc36bc1e02506f63dba252d
Luke Daley <ld@ldaley.com>
2011-08-29 18:08:29 +0100

    Version 0.2

--- FunctionalTestDevelopmentGrailsPlugin.groovy
+++ FunctionalTestDevelopmentGrailsPlugin.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 class FunctionalTestDevelopmentGrailsPlugin {
-	def version = "0.1.9-SNAPSHOT"
+	def version = "0.2"
 	def grailsVersion = "* > 1.3.5"
 	def dependsOn = [:]
 	def pluginExcludes = [
